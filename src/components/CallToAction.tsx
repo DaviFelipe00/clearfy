af05b1a3b46a880b592d0a27476bb6c505cb2975
davifelipe00 <davifelipeempregos@gmail.com>
2025-12-16 18:47:24 -0300

    corrijindo bug do whatsapp

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -25,14 +25,14 @@ export function CallToAction() {
           >
             Garantir Minha Profissional
           </Link>
-          <Link
-            href="https://wa.me/S5581987000534"
+          <a // Mudei de Link para a
+            href="https://wa.me/558187000534"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center justify-center bg-white/20 text-white px-7 py-3 rounded-lg font-extrabold text-lg border-2 border-white hover:bg-white/30 transition-all duration-300"
-          >
-            Falar no WhatsApp
-          </Link>
+          className="flex items-center justify-center bg-white/20 text-white px-7 py-3 rounded-lg font-extrabold text-lg border-2 border-white hover:bg-white/30 transition-all duration-300"
+>
+  Falar no WhatsApp
+</a>
         </div>
 
         <p className="mt-8 text-sm text-sky-600">
